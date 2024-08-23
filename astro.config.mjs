e6c18aa198f82e3c240d6fc77d8fa98dae3c1523
Bivekich <biveki-dev@yandex.ru>
2024-08-23 05:22:05 +0300

    new ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,18 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sanity from '@sanity/astro';
 
-import netlify from "@astrojs/netlify";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), sanity({
-    projectId: '20ujrfez',
-    dataset: 'production',
-    useCdn: true
-  })],
-  output: "server",
-  adapter: netlify()
+  integrations: [
+    react(),
+    sanity({
+      projectId: '20ujrfez',
+      dataset: 'production',
+      useCdn: true,
+    }),
+  ],
+  output: 'server',
+  adapter: vercel(),
 });
