b09b59a7254406c095a6ab99869b656bb0fa86c7
Lev Danilov <biveki-dev@yandex.ru>
2024-09-04 14:36:33 +0300

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sanity from "@sanity/astro";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    sanity({
-      projectId: "20ujrfez",
-      dataset: "production",
-      useCdn: true,
-    }),
-  ],
+  integrations: [react(), sanity({
+    projectId: "20ujrfez",
+    dataset: "production",
+    useCdn: true
+  }), sitemap()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
